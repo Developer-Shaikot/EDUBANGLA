11212ba902445bc165be0d092d55b384e20b6036
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-07-07 11:34:21 +0600

    modified: App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -35,62 +35,62 @@ const App = () => {
 	const isLoggedInfo = useIsLoggedInQuery();
 
 	return (
-		<div className={currentMode === "Dark" ? "dark" : ""}>
-			<BrowserRouter>
-				<div className="flex relative dark:bg-main-dark-bg">
-					<div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-						<TooltipComponent content="Settings" position="Top">
-							<button
-								type="button"
-								onClick={() => setThemeSettings(true)}
-								style={{ background: currentColor, borderRadius: "50%" }}
-								className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
-							>
-								<FiSettings />
-							</button>
-						</TooltipComponent>
-					</div>
-					{isLoggedInfo.isSuccess && isLoggedInfo.data?.status && (
-						<>
-							{activeMenu ? (
-								<div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-									<Sidebar />
-								</div>
-							) : (
-								<div className="w-0 dark:bg-secondary-dark-bg">
-									<Sidebar />
-								</div>
-							)}
-						</>
-					)}
-					<div
-						className={
-							activeMenu
-								? "dark:bg-main-dark-bg  bg-main-bg min-h-screen w-full " +
-								  (isLoggedInfo?.data?.status && "md:ml-72")
-								: "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
-						}
-					>
-						<div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-							<Navbar />
-						</div>
-						<div>
-							{isLoggedInfo.isSuccess && isLoggedInfo.data?.status && (
-								<ChatContainer />
-							)}
+    <div className={currentMode === "Dark" ? "dark" : ""}>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: "50%" }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {isLoggedInfo.isSuccess && isLoggedInfo.data?.status && (
+            <>
+              {activeMenu ? (
+                <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-light-gray ">
+                  <Sidebar />
+                </div>
+              ) : (
+                <div className="w-0 dark:bg-secondary-dark-bg">
+                  <Sidebar />
+                </div>
+              )}
+            </>
+          )}
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen w-full " +
+                  (isLoggedInfo?.data?.status && "md:ml-72")
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+            <div>
+              {isLoggedInfo.isSuccess && isLoggedInfo.data?.status && (
+                <ChatContainer />
+              )}
 
-							{themeSettings && <ThemeSettings />}
+              {themeSettings && <ThemeSettings />}
 
-							{/* all routes */}
-							<Routers />
-						</div>
-						<Footer />
-					</div>
-				</div>
-			</BrowserRouter>
-			<ToastContainer autoclose={4000} />
-		</div>
-	);
+              {/* all routes */}
+              <Routers />
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </BrowserRouter>
+      <ToastContainer autoclose={4000} />
+    </div>
+  );
 };
 
 export default App;
