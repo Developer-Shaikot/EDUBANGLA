9f4665443b94a81ad6c79369609075f28b33aef4
Minhaz <sharif35-3001@diu.edu.bd>
2023-07-14 00:32:15 +0600

    update: cors url

--- server/middlewares/middlewares.js
+++ server/middlewares/middlewares.js
@@ -3,16 +3,16 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const middlewares = [
-  cors({
-    credentials: true,
-    origin: "https://edu-bangla.onrender.com",
-  }),
-  cookieParser(),
-  express.static("public"),
-  express.json(),
-  express.urlencoded({ extended: true }),
+	cors({
+		credentials: true,
+		origin: ["https://edubangla-333a0.web.app", "http://localhost:3000"],
+	}),
+	cookieParser(),
+	express.static("public"),
+	express.json(),
+	express.urlencoded({ extended: true }),
 ];
 
 module.exports = (app) => {
-  app.use(middlewares);
+	app.use(middlewares);
 };
